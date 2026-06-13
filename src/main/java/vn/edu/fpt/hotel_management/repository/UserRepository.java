19a93ca1de6a59c78f9a8943f7d556aef02fdc83
Xiaohei21706 <thanhmhd123@gmail.com>
2026-06-13 09:45:44 +0700

    Model rename thành entity

--- src/main/java/vn/edu/fpt/hotel_management/repository/UserRepository.java
+++ src/main/java/vn/edu/fpt/hotel_management/repository/UserRepository.java
@@ -2,7 +2,7 @@
 package vn.edu.fpt.hotel_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.fpt.hotel_management.model.User;
+import vn.edu.fpt.hotel_management.entity.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
