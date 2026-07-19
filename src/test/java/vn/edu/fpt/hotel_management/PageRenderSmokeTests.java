492e1abe2c5f9c608a7d23b3616940e7242a4500
LHB16 <luuhuubinh21@gmail.com>
2026-07-19 16:48:21 +0700

    BinhLH_Update UI templates and project dependencies
    - Added navBar and footer fragments to auth/reset-password.html
    - Added footer fragment to booking/cancel-policy.html
    - Updated pom.xml Spring Boot version and dependencies
    - Fixed AutoConfigureMockMvc import in PageRenderSmokeTests.java
    - Updated test data and reports (booking, wishlist)
    - Added new banner image (Invicon_Emerald.png)
    - Cleaned up temporary diff text files

--- src/test/java/vn/edu/fpt/hotel_management/PageRenderSmokeTests.java
+++ src/test/java/vn/edu/fpt/hotel_management/PageRenderSmokeTests.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import vn.edu.fpt.hotel_management.entity.User;
